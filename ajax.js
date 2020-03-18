ad69056ea5fc61cceefc175446361d8a9394cb0f
beardykraken <vcasmirri@gmail.com>
2020-03-17 20:10:22 -0400

    Edited API routes to include posting methods that catch object information from form entries.

--- ajax.js
+++ ajax.js
@@ -1,47 +1,63 @@
-var tables = [];
-var waitlist = [];
 
+  var tables = [];
+  var waitlist = [];
 
 // Routes
 // =============================================================
 
 module.exports = function(app) {
-    
-}
+  // Create New Tables - takes in JSON input
 
-  // Displays a single character, or returns false
-  app.get("/api/characters/:character", function(req, res) {
-    var chosen = req.params.character;
-  
-    console.log(chosen);
-  
-    for (var i = 0; i < characters.length; i++) {
-      if (chosen === characters[i].routeName) {
-        return res.json(characters[i]);
-      }
-    }
-  
-    return res.json(false);
-  });
-  
-  // Create New Characters - takes in JSON input
-  app.post("/api/characters", function(req, res) {
+
+  if (tables.length > 4) {
+      postWaitlist();
+  } else {
+    postTables();
+  }
+  function postTables () {
+      app.post("/api/tables", function(req, res) {
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body parsing middleware
-    var newcharacter = req.body;
+    var newReservation = req.body;
   
-    console.log(newcharacter);
+    // Using a RegEx Pattern to remove spaces from newCharacter
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    newReservation.routeName = newReservation.name.replace(/\s+/g, "").toLowerCase();
   
-    // We then add the json the user sent to the character array
-    characters.push(newcharacter);
+    console.log(newReservation);
   
-    // We then display the JSON to the users
-    res.json(newcharacter);
+    tables.push(newReservation);
+  
+    res.json(newReservation);
+
+    return res.json(tables);
   });
+};
+
+function postWaitlist () {
+    app.post("/api/waitlist", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body parsing middleware
+  var newReservation = req.body;
+
+  // Using a RegEx Pattern to remove spaces from newCharacter
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  newReservation.routeName = newReservation.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newReservation);
+
+  waitlist.push(newReservation);
+
+  res.json(newReservation);
+
+  return res.json(waitlist);
+});
+};
   
   // Starts the server to begin listening
   // =============================================================
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
-  
+}
+
--- apiRoutes.js
+++ apiRoutes.js
@@ -9,15 +9,52 @@ var waitlist = require('????');
 // =============================================================
 module.exports = function(app) {
 
-    // A GET route with the url /api/tables. This will be used to display a JSON of all possible reservations.
-    app.get("/api/tables", function(req, res) {
-        return res.json(tables);
-    });
+    // If the tables array already has five reservations, add new reservations to the waitlist.
+    if (tables.length > 4) {
+        postWaitlist();
+    } else {
+      postTables();
+    }
 
-    // A GET route with the url /api/tables. This will be used to display a JSON of all those on a waitlist.
-    app.get("/api/waitlist", function(req, res) {
-        return res.json(waitlist);
+    function postTables () {
+        app.post("/api/tables", function(req, res) {
+      // req.body hosts is equal to the JSON post sent from the user
+      // This works because of our body parsing middleware
+      var newReservation = req.body;
+    
+      // Using a RegEx Pattern to remove spaces from newCharacter
+      // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+      newReservation.routeName = newReservation.name.replace(/\s+/g, "").toLowerCase();
+    
+      console.log(newReservation);
+    
+      tables.push(newReservation);
+    
+      res.json(newReservation);
+  
+      return res.json(tables);
     });
+  };
+  
+  function postWaitlist () {
+      app.post("/api/waitlist", function(req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    var newReservation = req.body;
+  
+    // Using a RegEx Pattern to remove spaces from newCharacter
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    newReservation.routeName = newReservation.name.replace(/\s+/g, "").toLowerCase();
+  
+    console.log(newReservation);
+  
+    waitlist.push(newReservation);
+  
+    res.json(newReservation);
+  
+    return res.json(waitlist);
+  });
+  };
 
 };
 // =============================================================
